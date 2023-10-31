21fef1990a2230fd560fc0b94500d4832c20bafc
bwa <40144023+beaesq@users.noreply.github.com>
2023-11-01 01:15:47 +0800

    Rename hits to hitTotal, add hit() and isSunk()

--- src/ship.js
+++ src/ship.js
@@ -1,3 +1,11 @@
-export default function createShip (length, hits = 0, isSunk = false) {
-  return { length, hits, isSunk }
+export default function createShip(length, startingHits = 0) {
+  let hits = startingHits;
+
+  const isSunk = () => length === hits;
+
+  const hit = () => { 
+    if (hits < length) hits += 1;
+  }
+
+  return { length, get hitTotal() { return hits; }, isSunk, hit }
 }
