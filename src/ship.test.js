8bc1e445a58cbe3d95777027593f3bf882377f0f
bwa <40144023+beaesq@users.noreply.github.com>
2023-11-02 03:02:20 +0800

    Convert isSunk from function to boolean

--- src/ship.test.js
+++ src/ship.test.js
@@ -17,16 +17,16 @@ describe('hit()', () => {
 describe('isSunk()', () => {
   it('ship is sunk when it is fully hit', () => {
     const ship = createShip(4, 4);
-    expect(ship.isSunk()).toBeTruthy();
+    expect(ship.isSunk).toBeTruthy();
   });
 
   it('ship is not sunk when it is partially hit', () => {
     const ship = createShip(5, 2);
-    expect(ship.isSunk()).toBeFalsy();
+    expect(ship.isSunk).toBeFalsy();
   });
 
   it('ship is not sunk when it has not been hit', () => {
     const ship = createShip(2, 0);
-    expect(ship.isSunk()).toBeFalsy();
+    expect(ship.isSunk).toBeFalsy();
   });
 });
