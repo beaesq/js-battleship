58c3f3b5d2921889ea6756b781757d97fc5a53b8
bwa <40144023+beaesq@users.noreply.github.com>
2023-11-02 06:56:48 +0800

    Add random move maker

--- src/player.js
+++ src/player.js
@@ -1,7 +1,36 @@
 export default function createPlayer(createGameboard, name = "Chuu", isHuman = false) {
   const gameboard = createGameboard();
+  const moves = [];
 
+  function compare(arr1, arr2) {
+    // assumes same number of elements
+    for (let index = 0; index < arr1.length; index += 1) {
+      if (arr1[index] !== arr2[index]) {
+        return false;
+      }
+    }
+    return true;
+  }
 
+  function includesArray(sourceArray, searchElement) {
+    // assumes same number of elements
+    for (let index = 0; index < sourceArray.length; index += 1) {
+      if (compare(sourceArray[index], searchElement)) return true;
+    }
+    return false;
+  }
 
-  return { name, gameboard }
+  function makeMove(size = 10) {
+    let move = [];
+    do {
+      move = [Math.floor(Math.random() * size), Math.floor(Math.random() * size)];
+      if (!includesArray(moves, move)) {
+        moves.push(move);
+        break;
+      }
+    } while (moves.length < size * size); // infinite loop failsafe? idk
+    return move;
+  }
+
+  return { name, gameboard, makeMove }
 }
