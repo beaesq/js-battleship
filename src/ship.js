59a14c32d7b10dc54c23fa6427fabde466593d7c
bwa <40144023+beaesq@users.noreply.github.com>
2023-11-02 03:08:09 +0800

    Change isSunk back to function for consistency

--- src/ship.js
+++ src/ship.js
@@ -1,11 +1,11 @@
 export default function createShip(length, startingHits = 0) {
   let hits = startingHits;
 
-  // const isSunk = () => length === hits;
+  const isSunk = () => length === hits;
 
   const hit = () => { 
     if (hits < length) hits += 1;
   }
 
-  return { length, get hitTotal() { return hits; }, get isSunk() { return length === hits; }, hit }
+  return { length, get hitTotal() { return hits; }, isSunk, hit }
 }
