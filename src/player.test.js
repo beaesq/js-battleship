e64231423a996e4e5120c26c97007a7debb3181d
bwa <40144023+beaesq@users.noreply.github.com>
2023-11-02 05:29:25 +0800

    Add tests for gameboard making and computer player

--- src/player.test.js
+++ src/player.test.js
@@ -1,2 +1,31 @@
 import createPlayer from "./player";
 
+it('makes gameboard on creation', () => { 
+  const mockCreateGameboard = jest.fn();
+  mockCreateGameboard.mockReturnValue('board');
+  const player = createPlayer(mockCreateGameboard, 'gowon', true);
+  expect(player.gameboard).toBe('board');
+});
+
+describe('computer player', () => {
+  let mockCreateGameboard;
+
+  beforeEach(() => { 
+    mockCreateGameboard = jest.fn();
+    jest.spyOn(global.Math, 'random').mockReturnValueOnce(0.123456789).mockReturnValueOnce(0.123456789).mockReturnValueOnce(0.123456789).mockReturnValue(0.999999999);
+  });
+
+  it('returns a valid move', () => { 
+    const computer = createPlayer(mockCreateGameboard);
+    expect(computer.makeMove()).toEqual([1, 1]);
+  });
+
+  it('does not repeat moves', () => { 
+    const computer = createPlayer(mockCreateGameboard);
+    const moves = [];
+    moves.push(computer.makeMove());
+    moves.push(computer.makeMove());
+    expect(moves).toHaveLength(2);
+    expect(moves).toEqual([[1, 1], [5, 5]]);
+  });
+});
