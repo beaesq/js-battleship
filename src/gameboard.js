7c4744588c6578f9933d1f2fc6f51414b3f4971a
bwa <40144023+beaesq@users.noreply.github.com>
2023-11-01 23:45:26 +0800

    Add ship overlap checker

--- src/gameboard.js
+++ src/gameboard.js
@@ -12,8 +12,51 @@ export default function createGameboard(size = 10) {
     return false;
   }
 
+  function compare(arr1, arr2) {
+    // assumes same number of elements
+    for (let index = 0; index < arr1.length; index += 1) {
+      if (arr1[index] !== arr2[index]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function includesArray(sourceArray, searchElement) {
+    // assumes same number of elements
+    for (let index = 0; index < sourceArray.length; index += 1) {
+      if (compare(sourceArray[index], searchElement)) return true;
+    }
+    return false;
+  }
+
+  function isOverOtherShips([x, y], length, isVertical) {
+    // make coordinate array of new ship
+    const shipCoordArr = [];
+    for (let index = 0; index < length; index += 1) {
+      const [a, b] = isVertical ? [x, y + index] : [x + index, y];
+      shipCoordArr.push([a, b]);
+    }
+
+    // go through all existing ships
+    for (let i = 0; i < ships.length; i += 1) {
+      const currentShip = ships[i];
+      const [currentShipX, currentShipY] = currentShip.coordinates;
+
+      // go through coordinate array of current ship
+      for (let j = 0; j < currentShip.ship.length; j += 1) {
+        const [a, b] = currentShip.isVertical ? [currentShipX, currentShipY + j] : [currentShipX + j, currentShipY];
+
+        // check if any coordinates of the new ship overlap with the current ship
+        if (includesArray(shipCoordArr, [a, b])) return true;
+      }
+    }
+    return false;
+  }
+
   function place(createShip, length, coordinates, isVertical) {
     if (isOutsideBoard(coordinates, length, isVertical)) return;
+    if (isOverOtherShips(coordinates, length, isVertical)) return;
 
     const ship = createShip(length);
     const shipObj = {
