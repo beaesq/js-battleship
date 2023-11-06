9af73d7fefb5569b069d5fad891876aef6735d04
bwa <40144023+beaesq@users.noreply.github.com>
2023-11-06 08:42:13 +0800

    Move includes fn to separate module

--- src/player.js
+++ src/player.js
@@ -1,23 +1,7 @@
+import { includesArray } from "./includesArray";
+
 export default function createPlayer(gameboard, name = "Chuu") {
   const moves = [];
-
-  function compare(arr1, arr2) {
-    // assumes same number of elements
-    for (let index = 0; index < arr1.length; index += 1) {
-      if (arr1[index] !== arr2[index]) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  function includesArray(sourceArray, searchElement) {
-    // assumes same number of elements
-    for (let index = 0; index < sourceArray.length; index += 1) {
-      if (compare(sourceArray[index], searchElement)) return true;
-    }
-    return false;
-  }
 
   function makeMove(size = 10) {
     let move = [];
